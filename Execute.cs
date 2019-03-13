4b01f9d1621548f42c1be596cf8dfbbe8d6d0053
milkitic <milkitic@126.com>
2019-03-13 21:54:28 +0800

    Initial commit

---
+++ Execute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Milky.WpfApi
+{
+    public static class Execute
+    {
+        public static void OnUiThread(this Action action)
+        {
+            Application.Current.Dispatcher.Invoke(() => { action?.Invoke(); });
+        }
+    }
+}
