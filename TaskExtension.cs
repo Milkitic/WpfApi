1cb572391889bf4ec83d3346b16a9b6c506f5008
milkitic <milkitic@126.com>
2019-03-15 22:13:31 +0800

    Create TaskExtension.cs

---
+++ TaskExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milky.WpfApi
+{
+    public static class TaskExtension
+    {
+        public static bool IsTaskFree(this Task task)
+        {
+            return task != null && (task.IsCanceled || task.IsCompleted || task.IsFaulted);
+        }
+
+        public static bool IsTaskBusy(this Task task)
+        {
+            return task != null && !task.IsCanceled && !task.IsCompleted && !task.IsFaulted;
+        }
+    }
+}
