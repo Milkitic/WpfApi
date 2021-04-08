61e6dcf3292f696baa228b663f951abd797d5da8
Milkitic <Milkitic@126.com>
2021-04-08 11:05:59 +0800

    change some

--- Execute.cs
+++ Execute.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Threading;
 
 namespace Milki.Utils.WPF
 {
@@ -45,7 +44,6 @@ namespace Milki.Utils.WPF
             }
         }
 
-        [Obsolete("use OnUiThreadAsync() instead", true)]
         public static void OnUiThread(this Action action)
         {
             if (Application.Current?.Dispatcher != null)
@@ -57,76 +55,6 @@ namespace Milki.Utils.WPF
                 try
                 {
                     action?.Invoke();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("UiContext execute error: " + ex.Message);
-                }
-            }
-        }
-
-        //public static async Task OnUiThreadAsync(this Func<Task> action)
-        //{
-        //    if (_uiContext == null)
-        //    {
-        //        if (Application.Current?.Dispatcher != null)
-        //        {
-        //            await Application.Current.Dispatcher.InvokeAsync(action);
-        //        }
-        //        else
-        //        {
-        //            try
-        //            {
-        //                if (action != null) await action();
-        //            }
-        //            catch (Exception ex)
-        //            {
-        //                Console.WriteLine("UiContext execute error: " + ex.Message);
-        //            }
-        //        }
-        //    }
-        //    else
-        //    {
-        //        _uiContext.Send(obj => { action?.Invoke(); }, null);
-        //    }
-        //}
-
-        [Obsolete("use OnUiThreadAsync() instead", true)]
-        public static void ToUiThread(this Action action, Action callBack = null)
-        {
-            if (Application.Current?.Dispatcher != null)
-            {
-                object executed = false;
-                object @lock = new object();
-                var result = Application.Current?.Dispatcher?.BeginInvoke(action, DispatcherPriority.Normal);
-
-                result.Completed += (sender, e) =>
-                {
-                    lock (@lock)
-                    {
-                        if ((bool)executed) return;
-                        executed = true;
-                    }
-
-                    callBack?.Invoke();
-                };
-
-                if (result.Status == DispatcherOperationStatus.Completed)
-                {
-                    lock (@lock)
-                    {
-                        if ((bool)executed) return;
-                        executed = true;
-                    }
-
-                    callBack?.Invoke();
-                }
-            }
-            else
-            {
-                try
-                {
-                    action?.BeginInvoke(ar => callBack?.Invoke(), null);
                 }
                 catch (Exception ex)
                 {
